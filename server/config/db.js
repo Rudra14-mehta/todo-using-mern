c1b7de8bf75b24267b87808fdffd8a7d6f53409e
Rudra Mehta <124079761+Rudra14-mehta@users.noreply.github.com>
2025-07-10 12:20:20 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://rudra141020:GbdbpODVr3LGWvC1@cluster0.tzbqkne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+await mongoose.connect('mongodb+srv://erprathamsaxena:nkOh7iKaC4ddwaP9@cluster0.tzbqkne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
